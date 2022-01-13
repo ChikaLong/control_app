d35623c27aa0395d29b8ff45372ce78388720b30
ChikaLong <longb.0818@gmail.com>
2022-01-13 14:38:36 +0000

    [Add]ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'users#new'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
 
   resources :users
 end
